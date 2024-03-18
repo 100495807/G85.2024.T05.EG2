935059859b9ec0f49b0c9250cd0ae66dd4a9ef3e
100495807 <158489101+100495807@users.noreply.github.com>
2024-03-18 15:35:57 +0100

    instalacion

--- src/main/python/UC3MTravel/HotelManager.py
+++ src/main/python/UC3MTravel/HotelManager.py
@@ -8,6 +8,41 @@ class HotelManager:
         pass
 
     def validatecreditcard(self, x):
+        """
+        Valida la validez de una tarjeta de crédito utilizando el algoritmo de Luhn.
+        Argumentos:
+          x: El número de la tarjeta de crédito como cadena.
+        Retorno:
+          True si la tarjeta de crédito es válida, False si no.
+        """
+
+        # Invertir el número
+        reversed_number = x[::-1]
+
+        # Lista para almacenar los dígitos duplicados
+        doubled_digits = []
+
+        # Duplicar dígitos pares
+        for i, digit in enumerate(reversed_number):
+            if i % 2 == 1:
+                doubled_digits.append(int(digit) * 2)
+            else:
+                doubled_digits.append(int(digit))
+
+        # Lista para almacenar los dígitos finales
+        final_digits = []
+
+        # Sumar dígitos
+        for digit in doubled_digits:
+            if digit <= 9:
+                final_digits.append(digit)
+            else:
+                final_digits.append(digit - 9)
+
+        sum_digits = sum(final_digits)
+
+        # Validar el número
+        return sum_digits % 10 == 0
 
 
     def ReaddatafromJSOn(self, fi):
