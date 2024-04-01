57989a77be1c29b0f41ae3e2344a9d170593ce1c
100495807 <158489101+100495807@users.noreply.github.com>
2024-04-01 12:22:53 +0200

    Update test_prueba_tests.py

--- src/unittest/python/test_prueba_tests.py
+++ src/unittest/python/test_prueba_tests.py
@@ -1,18 +1,9 @@
 import unittest
 
-from prueba import sum
+
 from unittest import TestCase
 
-class TestDeliverProduct(TestCase):
-        def setUp(self):
-            pass
-        def test_hotel_reservation_uk1(self):
-            pass
 
-
-class TestPrueba(TestCase):
-    def test_sum(self):
-        self.assertEqual(sum(7,5), 12)
 
 if __name__ == "__main__":
     unittest.main()
