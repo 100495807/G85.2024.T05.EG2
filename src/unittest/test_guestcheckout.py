8dae66e02c89e6dda3c64bfb88dc7f45220f2cf2
100495807 <158489101+100495807@users.noreply.github.com>
2024-04-02 12:17:48 +0200

    summary_uni_f2

--- src/unittest/test_guestcheckout.py
+++ src/unittest/test_guestcheckout.py
@@ -3,18 +3,20 @@ from UC3MTravel.HotelManager import HotelManager
 from pathlib import Path
 import unittest
 from freezegun import freeze_time
+
 JSON_FILES_PATH = str(Path.home()) + "/PycharmProjects/G85.2024.T05.EG2/src/JsonFiles/"
+
 
 
 class TestGuestCheckout(unittest.TestCase):
     def setUp(self):
         self.manager = HotelManager()  # Crear una instancia de HotelManager
 
-    @freeze_time("06/04/2024")
+    @freeze_time("18/12/2024")
     def test_guest_checkout_valid(self):
         # Caso de prueba válido
-        self.assertTrue(self.manager.guest_checkout("28d32fa25abce85a4f01bebd32cf6a7364f532727af401c1561eefec02cf1cf9") == True)
-
+        self.assertTrue(
+            self.manager.guest_checkout("aafaff12f5fb742529f6d794d3e251f6387cd8633b5d3d29825afba6564d1581") == True)
 
     def test_guest_checkout_invalid_room_key(self):
         # Casos de prueba con room_key inválido
@@ -40,6 +42,7 @@ class TestGuestCheckout(unittest.TestCase):
         # Caso de prueba con fecha de salida en el pasado
         with self.assertRaises(HotelManagementException):
             self.manager.guest_checkout("room_key_with_past_date")
+
 
 if __name__ == '__main__':
     unittest.main()
